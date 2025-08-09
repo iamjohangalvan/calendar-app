f9b97b727f06fec3b8907e0accc4638f107de4ec
iamjohangalvan <iamjohangalvan@gmail.com>
2025-08-09 06:51:22 -0400

    calendar backend

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+require("dotenv").config();
+console.log(process.env);
+
+// Crear servidor express
+const app = express();
+
+// Directorio Publico
+app.use(express.static("public"));
+
+// Lectura y parseo del body
+app.use(express.json());
+
+// Rutas
+app.use("/api/auth", require("./routes/auth"));
+
+// Escuchar peticiones
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
+});
